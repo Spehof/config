388eeaa3325aa7d113f344fe678080e18f15a03b
Roman Spehov <volkovsws@gmail.com>
2021-10-19 01:33:33 +0300

    add aliases for editing git configs

--- .config/sf.environment/alias.d/sys_configs.sh
+++ .config/sf.environment/alias.d/sys_configs.sh
@@ -3,4 +3,5 @@ alias vc='vim $HOME/.vimrc'
 alias rc='vim $HOME/.config/ranger/rc.conf'
 alias ic='vim $HOME/.config/i3/config'
 alias pc='vim $HOME/.config/polybar/config'
-
+alias gc='vim $HOME/.gitconfig'
+alias gic='vim $HOME/.gitignore'
